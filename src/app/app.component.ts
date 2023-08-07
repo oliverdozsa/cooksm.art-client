9305a8c1bfca8abcb010941b76953a23c35c1f3d
Oliver Dozsa <dozsa.oliver@gmail.com>
2023-08-07 19:54:14 +0200

    Navbar items.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {RouteNames} from "./route-names";
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'cooksm.art-client';
-
   isMenuCollapsed = true;
+  routes = [
+    {name: "Home", link: RouteNames.HOME},
+    {name: "About", link: RouteNames.ABOUT},
+  ]
 }
