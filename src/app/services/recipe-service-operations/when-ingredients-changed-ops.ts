36d04e856c16c623a0daaf60d79585a8fa8fef7d
Oliver Dozsa <dozsa.oliver@gmail.com>
2023-09-22 21:40:59 +0200

    Refactored handle extra relations ops when ingredients change.

--- src/app/services/recipe-service-operations/when-ingredients-changed-ops.ts
+++ src/app/services/recipe-service-operations/when-ingredients-changed-ops.ts
@@ -1,12 +1,12 @@
 import {SearchSnapshot} from "../../data/search-snapshot";
 import {SearchSnapshotUpdate} from "../../data/search-snapshot-ops/search-snapshot-update";
 import {AppSearchMode} from "../../data/app-search-mode";
-import {determineAppSearchMode, determineExtraRelation} from "../../data/saved-recipe-search";
+import {determineAppSearchMode} from "../../data/saved-recipe-search";
 import {Subject} from "rxjs";
 import {RecipeServiceOperation, RecipeServiceOperationType} from "../recipe-service-operation";
 import {DisabledSearchModes} from "./disabled-search-modes";
 import {RecipeServiceCommonOps} from "./recipe-service-common-ops";
-import {ExtraRelation} from "../../data/extra-ingredients";
+import {WhenIngredientsChangedHandleExtraRelationsOps} from "./when-ingredients-changed-handle-extra-relations-ops";
 
 export class WhenIngredientsChangedOps {
   private disabledSearchModes: DisabledSearchModes;
@@ -56,94 +56,8 @@ export class WhenIngredientsChangedOps {
   }
 
   handleExtraRelationAdjustments() {
-    const query = this.snapshotForCurrentQuery.search.query;
-
-    const maxAllowedTotalIngredients = this.calcMaxAllowedTotalExtraIngredients();
-    const isMoreExtraIngredientsAddedThanAllowed = query.goodAdditionalIngs != undefined &&
-      query.goodAdditionalIngs > maxAllowedTotalIngredients;
-
-    const shouldExtraRelationBeEnabled = this.shouldExtraRelationBeEnabled();
-    this.enableExtraRelations(shouldExtraRelationBeEnabled);
-    if (shouldExtraRelationBeEnabled) {
-      this.setExtraIngredientsMaxOptionTo(maxAllowedTotalIngredients);
-    }
-
-    let relationToUse = determineExtraRelation(query);
-    if(relationToUse === undefined) {
-      relationToUse = ExtraRelation.CanBeMoreThan;
-    }
-
-    let relationValueToUse = isMoreExtraIngredientsAddedThanAllowed ?
-      maxAllowedTotalIngredients : query.goodAdditionalIngs
-    if(relationValueToUse == undefined) {
-      relationValueToUse = 1;
-    }
-
-    if(shouldExtraRelationBeEnabled) {
-      SearchSnapshotUpdate.withExtraRelation(relationToUse, relationValueToUse, this.snapshotForCurrentQuery);
-    } else {
-      SearchSnapshotUpdate.clearExtraRelation(this.snapshotForCurrentQuery);
-    }
-
-    this.setExtraIngredientsRelationAndValueTo(relationToUse, relationValueToUse);
-  }
-
-  private shouldExtraRelationBeEnabled() {
-    const query = this.snapshotForCurrentQuery.search.query;
-
-    const isAdditionalIngredientsPresent = query.addIngs != undefined && query.addIngs.length > 0;
-    const isAdditionalIngredientTagsPresent = query.addIngTags != undefined && query.addIngTags.length > 0;
-
-    return isAdditionalIngredientsPresent || isAdditionalIngredientTagsPresent;
-  }
-
-  private calcMaxAllowedTotalExtraIngredients() {
-    const sumOfTotalIngredients = this.countTotalExtraIngredients();
-    if(sumOfTotalIngredients > 20) {
-      return 20;
-    }
-
-    if(sumOfTotalIngredients < 1) {
-      return 1;
-    }
-
-    return sumOfTotalIngredients;
-  }
-
-  private countTotalExtraIngredients() {
-    const query = this.snapshotForCurrentQuery.search.query;
-    let sumOfIngredients = query.addIngs == undefined ? 0 : query.addIngs.length;
-    let sumOfIngredientsOfCategories = query.addIngTags == undefined ? 0 :
-      query.addIngTags.reduce((a, c) => a + c.ingredients.length, 0);
-    return sumOfIngredientsOfCategories + sumOfIngredients;
-  }
-
-  private setExtraIngredientsMaxOptionTo(value: number) {
-    this.operation$.next({
-      type: RecipeServiceOperationType.SetExtraIngredientsRelationOptions,
-      payload: {
-        maxOptions: value
-      }
-    });
-  }
-
-  private setExtraIngredientsRelationAndValueTo(relation: ExtraRelation, value: number) {
-    this.operation$.next({
-      type: RecipeServiceOperationType.SetExtraIngredientsRelation,
-      payload: {
-        value: value,
-        relation: relation
-      }
-    });
-  }
-
-  private enableExtraRelations(isEnabled: boolean) {
-    const operationType = isEnabled ?
-      RecipeServiceOperationType.EnableExtraIngredientsRelation : RecipeServiceOperationType.DisableExtraIngredientsRelation;
-
-    this.operation$.next({
-      type: operationType,
-      payload: undefined
-    });
+    const handleOps =
+      new WhenIngredientsChangedHandleExtraRelationsOps(this.snapshotForCurrentQuery, this.operation$);
+    handleOps.handleExtraRelationAdjustments();
   }
 }
