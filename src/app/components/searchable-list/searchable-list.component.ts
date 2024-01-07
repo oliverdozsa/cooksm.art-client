cda480b5881cc5b4cf31e875d81ba0e3794ebfc4
Oliver Dozsa <dozsa.oliver@gmail.com>
2024-01-07 11:27:02 +0100

    More work on searchable list.

--- src/app/components/searchable-list/searchable-list.component.ts
+++ src/app/components/searchable-list/searchable-list.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+
+export interface SearchableListItemControl {
+  icon: string;
+  onClick: (item: any) => void;
+}
 
 @Component({
   selector: 'app-searchable-list',
@@ -6,5 +11,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./searchable-list.component.scss']
 })
 export class SearchableListComponent {
+  @Input()
+  isWorking: boolean = false;
 
+  @Input()
+  nameKey: string = "name";
+
+  @Input()
+  itemControls: SearchableListItemControl[] = [
+    {icon: "bi-list-ul", onClick: i => console.log(`clicked on item through control ${JSON.stringify(i)}`)},
+    {icon: "bi-pen", onClick: i => console.log(`clicked on item through control ${JSON.stringify(i)}`)},
+    {icon: "bi-trash", onClick: i => console.log(`clicked on item through control ${JSON.stringify(i)}`)},
+  ];
+
+  @Input()
+  itemCreatable: boolean = false;
+
+  @Input()
+  onItemClick: (item: any) => void = () => {
+    console.log(`clicked on item`)
+  };
+
+  @Input()
+  onItemAddClick: () => void = () => {
+  };
+
+  @Input()
+  set items(value: any[]) {
+    this._items = value;
+    this.filteredItems = [...this._items];
+  }
+
+  filteredItems: any[] = [
+    {name: "alma"},
+    {name: "alak"},
+    {name: "bab ablak"}
+  ];
+
+  search: string = "";
+
+  private _items: any[] = [
+    {name: "alma"},
+    {name: "alak"},
+    {name: "bab ablak"}
+  ];
+
+  filterList() {
+    this.filteredItems = this._items
+      .filter(item => item[this.nameKey].toLowerCase().includes(this.search.toLowerCase()));
+  }
 }
