2ec8db579791c62d2427a2b4c342ea42545697ae
Oliver Dozsa <dozsa.oliver@gmail.com>
2024-03-14 17:52:47 +0100

    Saving user chosen locale.

--- src/app/services/language.service.ts
+++ src/app/services/language.service.ts
@@ -26,6 +26,7 @@ export class LanguageService {
     }
 
     this._usedLanguage = value;
+    localStorage.setItem("usedLocale", this.usedLanguageToLocale());
     this.changePath();
   }
 
@@ -34,7 +35,13 @@ export class LanguageService {
   }
 
   get usedLanguageId(): number {
-    return SearchSnapshotTransform.toLanguageId(this.searchSnapshotService.snapshot)
+    if(this.usedLanguage == Language.Hungarian) {
+      return 1;
+    } else if(this.usedLanguage == Language.English) {
+      return 2;
+    }
+
+    return 1;
   }
 
   private _usedLanguage;
@@ -44,6 +51,9 @@ export class LanguageService {
               private recipesService: RecipesService, private userService: UserService, private ingredientSearchService: IngredientSearchService,
               private spinnerService: NgxSpinnerService) {
 
+    const savedUsedLocale = localStorage.getItem("usedLocale");
+    const localeToUse = savedUsedLocale == null ? activeLocale : savedUsedLocale;
+
     this.snapshotTranslator = new SnapshotTranslator(ingredientSearchService);
 
     this.snapshotTranslator.translated$.subscribe({
@@ -51,11 +61,14 @@ export class LanguageService {
       error: () => this.translateSnapshotFinished()
     });
 
-    const activeLanguage = LanguageService.toLanguage(activeLocale);
-    this._usedLanguage = activeLanguage;
+    this._usedLanguage = LanguageService.toLanguage(localeToUse);
 
-    if (searchSnapshotService.cloneSnapshot().locale != activeLocale) {
+    if (searchSnapshotService.cloneSnapshot().locale != localeToUse) {
       this.translateSnapshot();
+    }
+
+    if(localeToUse != activeLocale) {
+      this.changePath();
     }
   }
 
